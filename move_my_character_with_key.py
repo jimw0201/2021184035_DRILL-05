44113b9f1fc6baecbd48d51f395eda0c1a511272
minwooji <jimw0201@gmail.com>
2024-09-30 22:26:53 +0900

    캐릭터를 그리고, 좌우 방향키로 움직이게 함

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,22 +5,39 @@ ground = load_image('TUK_GROUND.png')
 character = load_image('earthgirl.png')
 
 def handle_events():
-    global running
+    global running, x, dir
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir += 1
+            elif event.key == SDLK_LEFT:
+                dir -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
 
 running = True
 x = 1280 // 2
 y = 1024 // 2
 frame = 0
+dir = 0
 
 while running:
     clear_canvas()
     ground.draw(1280 // 2, 1024 // 2)
+    character.clip_draw(frame * 200, 200, 200, 200, x, y)
     update_canvas()
     handle_events()
+    frame = (frame + 1) % 8
+    x += dir * 5
+    delay(0.05)
 
 close_canvas()
