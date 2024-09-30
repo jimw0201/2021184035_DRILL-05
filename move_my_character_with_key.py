37e3e7a99d7940d7fa07670d5c913a48c4dac663
minwooji <jimw0201@gmail.com>
2024-09-30 23:14:16 +0900

    캐릭터 이미지가 이동 방향을 따라가게 적용

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -35,18 +35,41 @@ def handle_events():
 running = True
 x = 1280 // 2
 y = 1024 // 2
-frame = 0
+frame = 0           # 걷는 모션 프레임
+dir_frame = 200     # 걷는 방향 프레임
+flip = False        # 좌우 반전
 dir_x = 0
 dir_y = 0
 
 while running:
     clear_canvas()
-    ground.draw(1280 // 2, 1024 // 2)
-    character.clip_draw(frame * 200, 200, 200, 200, x, y)
-    x += dir_x * 5
-    y += dir_y * 5
+    ground.draw(640, 512)
+
+    if dir_y > 0:  # 윗쪽 이동 (셋째줄 이미지)
+        dir_frame = 0
+        flip = False
+    elif dir_y < 0:  # 아랫쪽 이동 (첫째줄 이미지)
+        dir_frame = 400
+        flip = False
+    elif dir_x > 0:  # 오른쪽 이동 (둘째줄 이미지)
+        dir_frame = 200
+        flip = True  # 좌우 반전 True
+    elif dir_x < 0:  # 왼쪽 이동 (둘째줄 이미지)
+        dir_frame = 200
+        flip = False
+
+    # 좌우 반전 여부에 따라 이미지 그리기
+    if flip:
+        character.clip_composite_draw(frame * 200, dir_frame, 200, 200, 0, 'h', x, y, 200, 200)
+    else:
+        character.clip_draw(frame * 200, dir_frame, 200, 200, x, y)
+
     update_canvas()
     handle_events()
+
+    x += dir_x * 5
+    y += dir_y * 5
+
     frame = (frame + 1) % 8
     delay(0.05)
 
